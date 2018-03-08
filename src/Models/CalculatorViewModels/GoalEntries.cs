d91938aa078ec265387017caf593d0223498f21b
Chris <cloos68@gmail.com>
2018-03-08 02:54:42 -0500

    Adding Goal item model

---
+++ src/Models/CalculatorViewModels/GoalEntries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TooBroke.Models.CalculatorViewModels
+{
+    public class GoalEntries
+    {
+        public int ID { get; set; }
+        public String ApplicationUserID { get; set; }
+        public int CategoryID { get; set; }
+        public String Title { get; set; }
+        public int InitialAmount { get; set; }
+        public int GoalAmount { get; set; }
+        public int Interest { get; set; }
+        public int Term { get; set; }
+        public int Compound { get; set; }
+        public int RecurringPayments { get; set; }
+        public int PaymentFrequency { get; set; }
+        public int TargetPayOff { get; set; }
+        public int GeneratedPayOff { get; set; }
+        public int OneTimePayment { get; set; }
+
+        public Categories Category { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
